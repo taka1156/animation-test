70aa388308e152bbaf29acd298ba07a6baaf9920
taka <tomo05061@gmail.com>
2023-08-03 01:36:36 +0900

    baseパス置き換え処理追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
+  base: mode !== 'development' ? '/animation-test/' : './',
   server: {
     port: 8080,
   },
   plugins: [react()],
-})
+}))
